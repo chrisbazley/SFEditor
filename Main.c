e5b08faccd924db10dc6309cc58ff6cd824fac47
Christopher Bazley <119876938+chrisbazley@users.noreply.github.com>
2026-08-28 00:10:04 +0100

    Fix DEBUGF call to include format specifier

--- Main.c
+++ Main.c
@@ -55,7 +55,7 @@ static void fortify_check(void)
 
 static void fortify_output(char const *text)
 {
-  DEBUGF(text);
+  DEBUGF("%s", text);
   if (strstr(text, "Fortify"))
   {
     assert(!fortify_detected);
