d4520be4889b90d94ce89ac40cd5b779748113ed
Christopher Bazley <119876938+chrisbazley@users.noreply.github.com>
2026-09-21 15:36:51 +0100

    Use void pointer conversions with intptr_t

--- SpecialShip.c
+++ SpecialShip.c
@@ -92,7 +92,8 @@ static void setup_win(EditSession *const session, ObjectId const dbox_id)
 
   E(stringset_set_selected(StringSet_IndexedSelection,
              dbox_id, ComponentId_SHIPTYPE,
-             (char *)(intptr_t)(player_get_ship_type(s) - ShipType_Player)));
+             (char *)(void *)(intptr_t)
+               (player_get_ship_type(s) - ShipType_Player)));
 
   E(optionbutton_set_state(0, dbox_id, ComponentId_ENABLE,
              player_get_equip_enabled(s)));
